0a9f7889776f1886b6e72e37d85cb324fd67ba18
mvnd06 <armand_r@icloud.com>
2023-03-30 14:44:28 +0000

    March Update

--- ros-i2cpwmboard-py/scripts/ros-i2cpwmboard.py
+++ ros-i2cpwmboard-py/scripts/ros-i2cpwmboard.py
@@ -10,6 +10,6 @@ if __name__ == '__main__':
 	rospy.init_node('i2cpwm_board_node_py')
 	rospy.loginfo('Python ROS PWM Controller Node Started!')
 
-	subscriber = rospy.Subscriber('servos_absolute', ServoArray, callback=receiveMessage)
+	subscriber = rospy.Subscriber('/servos_absolute', ServoArray, callback=receiveMessage)
 	rospy.spin()
 
