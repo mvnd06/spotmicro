f571e104512410d3d17c9e3cd14fede5dc0aecf2
Armand Raynor <13355630+mvnd06@users.noreply.github.com>
2023-03-30 13:33:20 -0700

    .

--- oled_display/scripts/oled_display.py
+++ oled_display/scripts/oled_display.py
@@ -36,12 +36,11 @@ class OLEDNode():
             rospy.spin()
     
     def color_callback(self, msg):
-        rospy.loginfo(f"Recieved message...")
+        rospy.loginfo(f"Recieved -> Red: {msg.r}, Green: {msg.g}, Blue: {msg.b}")
         if self.color_palette is None:
             self.color_palette = displayio.Palette(1)
         # Convert the RGB values to a single integer
         color_int = ((int(msg.r) << 16) | (int(msg.g) << 8) | int(msg.b))
-        rospy.loginfo(color_int)
         
         # Update the color palette with the new color
         self.color_palette[0] = color_int
