9259dec1391f2e7f9a512d98992b1bc9e909020d
Armand Raynor <13355630+mvnd06@users.noreply.github.com>
2023-03-31 00:48:13 -0700

    .

--- oled_display/scripts/oled_display.py
+++ oled_display/scripts/oled_display.py
@@ -60,8 +60,7 @@ class OLEDNode():
         rospy.loginfo("Starting animation...")
 
         # Load and display all BMP files in folder
-        script_dir = os.path.dirname(os.path.abspath(__file__))
-        folder_path = '~/catkin_ws/src/oled_display/scripts/resources/home_animation'
+        folder_path = '~/catkin_ws/src/oled_display/scripts/resources/home_animation/'
         bmp_files = [f for f in os.listdir(folder_path) if f.endswith('.bmp')]
 
         for bmp_file in bmp_files:
