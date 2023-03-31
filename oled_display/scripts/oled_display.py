9d72e3618425d2a7734cadb264feec3f07317750
Armand Raynor <13355630+mvnd06@users.noreply.github.com>
2023-03-31 00:29:16 -0700

    .

--- oled_display/scripts/oled_display.py
+++ oled_display/scripts/oled_display.py
@@ -40,7 +40,7 @@ class OLEDNode():
         self.color_sub = rospy.Subscriber('oled_color', ColorRGBA, self.color_callback)
 
         while not rospy.is_shutdown():
-            node.animation_callback("test")
+            self.animation_callback("test")
             rospy.spin()
     
     def color_callback(self, msg):
