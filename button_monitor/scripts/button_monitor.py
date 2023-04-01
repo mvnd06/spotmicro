95cd9da13c11e441d654166d671b3f9951b64cf5
Armand Raynor <13355630+mvnd06@users.noreply.github.com>
2023-03-31 19:24:39 -0700

    Button monitor

---
+++ button_monitor/scripts/button_monitor.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+import time
+import rospy
+from std_msgs.msg import Empty
+
+class ButtonNode:
+    def __init__(self, cooldown):
+        rospy.init_node('button_node', anonymous=True)
+        self.pub = rospy.Publisher('button_press', Empty, queue_size=10)
+        self.cooldown = cooldown
+        self.last_time = time.monotonic()
+        self.button_pin = 22
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.add_event_detect(self.button_pin, GPIO.RISING, callback=self.button_callback, bouncetime=50)
+
+    def button_callback(self, channel):
+        current_time = time.monotonic()
+        if current_time - self.last_time >= self.cooldown:
+            rospy.loginfo('Button was pushed')
+            self.pub.publish()
+            self.last_time = current_time
+
+    def run(self):
+        rospy.spin()
+        GPIO.cleanup()
+
+if __name__ == '__main__':
+    cooldown = rospy.get_param('~cooldown', 0.5)
+    button_node = ButtonNode(cooldown)
+    button_node.run()
