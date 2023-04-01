74cebe1d1204e285f961f3581b48918776e2328d
Armand Raynor <13355630+mvnd06@users.noreply.github.com>
2023-03-31 20:44:16 -0700

    .

--- display_manager/scripts/display_manager.py
+++ display_manager/scripts/display_manager.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 
 import rospy
-from std_msgs.msg import Int32MultiArray, ColorRGBA
+from std_msgs.msg import Int32MultiArray, ColorRGBA, Empty
 
 RED = ColorRGBA(r=255.0, g=0.0, b=0.0, a=1.0)
 GREEN = ColorRGBA(r=0.0, g=255.0, b=0.0, a=1.0)
 BLUE = ColorRGBA(r=0.0, g=0.0, b=255.0, a=1.0)
+PURPLE = ColorRGBA(r=128.0, g=0.0, b=128.0, a=1.0)
 BLACK = ColorRGBA(r=0.0, g=0.0, b=0.0, a=1.0)
+
+class ScreenMode(Enum):
+    ULTRASONIC = 1
+    STATIC = 2
 
 class DisplayManager:
     def __init__(self):
@@ -15,20 +20,39 @@ class DisplayManager:
 
         self.current_color = BLACK
         self.color_pub = rospy.Publisher('oled_color', ColorRGBA, queue_size=10)
+        self.screen_toggle = ScreenMode.ULTRASONIC
+
         rospy.Subscriber('ultrasonic_data', Int32MultiArray, self.ultrasonic_callback)
+        rospy.Subscriber('button_press', Empty, self.button_callback)
 
     def ultrasonic_callback(self, msg):
+        if self.screen_mode != ScreenMode.ULTRASONIC:
+            return
         # Parse the incoming message to extract the left and right distance values
         left_dist, right_dist = msg.data[0], msg.data[1]
         rospy.logdebug(f"Received data: {left_dist}, {right_dist}")
 
         # Determine if the distance is close enough to trigger an alert
-        if (left_dist < 5 or right_dist < 5) and self.current_color != RED:
-            # Send a message to the oled_color topic to update the color of the screen
-            self.color_pub.publish(RED)
-        elif self.current_color != GREEN:
-            self.color_pub.publish(GREEN)
+        if (left_dist < 5 or right_dist < 5):
+            self.current_color = RED
+        else:
+            self.current_color = GREEN
+        self.publish_color()
 
+    def static_screen(self):
+        self.current_color = PURPLE
+        self.publish_color()
+
+    def button_callback(self, msg):
+        if self.screen_mode == ScreenMode.ULTRASONIC:
+            self.screen_mode = ScreenMode.STATIC
+            self.static_screen()
+        else:
+            self.screen_mode = ScreenMode.ULTRASONIC
+
+    def publish_color(self):
+        self.color_pub.publish(self.current_color)
+        
 if __name__ == '__main__':
     try:
         display_manager = DisplayManager()
