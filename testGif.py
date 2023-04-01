ab0ad862dca840f77ddbc085fa330e5e4c1ba977
Armand Raynor <13355630+mvnd06@users.noreply.github.com>
2023-03-31 17:34:45 -0700

    .

--- testGif.py
+++ testGif.py
@@ -14,12 +14,16 @@ gif_image = Image.open(gif_path)
 
 # Extract individual frames from the gif
 frames = []
+frame_count = 0
 try:
     while True:
         frames.append(gif_image.copy())
         gif_image.seek(len(frames))
+        frame_count += 1
 except:
     pass
+
+print(f"Loaded {frame_count} frames")
 
 # Create the display
 spi = busio.SPI(clock=board.D11, MOSI=board.D10)
@@ -30,21 +34,24 @@ display_bus = displayio.FourWire(spi, command=tft_dc, chip_select=tft_cs, reset=
 display = SSD1331(display_bus, width=96, height=64)
 
 # Loop through the frames and display them on the screen
-while True:
-    for frame in frames:
-        # Convert the frame to a format that can be displayed on the screen
-        frame = frame.convert("RGB")
-        frame = frame.transpose(method=Image.TRANSPOSE)
+for i, frame in enumerate(frames):
+    print(f"Displaying frame {i+1} of {len(frames)}")
+    
+    # Convert the frame to a format that can be displayed on the screen
+    frame = frame.convert("RGB")
+    frame = frame.transpose(method=Image.TRANSPOSE)
 
-        # Load the frame onto the screen
-        bitmap, palette = adafruit_imageload.load(frame.tobytes(), bitmap=displayio.Bitmap, palette=displayio.Palette)
+    # Load the frame onto the screen
+    bitmap, palette = adafruit_imageload.load(frame.tobytes(), bitmap=displayio.Bitmap, palette=displayio.Palette)
 
-        sprite = displayio.TileGrid(bitmap, pixel_shader=palette)
-        group = displayio.Group(max_size=1)
-        group.append(sprite)
+    sprite = displayio.TileGrid(bitmap, pixel_shader=palette)
+    group = displayio.Group(max_size=1)
+    group.append(sprite)
 
-        display.show(group)
-        display.refresh()
+    display.show(group)
+    display.refresh()
 
-        # Wait for the next frame
-        time.sleep(0.1)
+    # Wait for the next frame
+    time.sleep(0.1)
+
+print("Animation complete")
