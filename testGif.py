8f034130dff7d9ce107500f32e7c9c35c5ea4024
Armand Raynor <13355630+mvnd06@users.noreply.github.com>
2023-03-31 17:41:44 -0700

    .

--- testGif.py
+++ testGif.py
@@ -42,9 +42,7 @@ for i, frame in enumerate(frames):
     frame = frame.transpose(method=Image.TRANSPOSE)
 
     # Load the frame onto the screen
-    with open(gif_path, "rb") as file:
-        print("File loaded")
-        bitmap, palette = adafruit_imageload.load(file, bitmap=displayio.Bitmap, palette=displayio.Palette)
+    bitmap, palette = adafruit_imageload.load(frame.tobytes(), bitmap=displayio.Bitmap, palette=displayio.Palette)
 
     print("Creating sprite")
     sprite = displayio.TileGrid(bitmap, pixel_shader=palette)
