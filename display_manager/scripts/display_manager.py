6f965631571543becd8635d1f6224f01b70c5e53
mvnd06 <armand_r@icloud.com>
2023-04-05 07:04:02 -0700

    move system_monitor to display_manager for now

--- display_manager/scripts/display_manager.py
+++ display_manager/scripts/display_manager.py
@@ -3,7 +3,50 @@
 import rospy
 from std_msgs.msg import Int32MultiArray, ColorRGBA, Empty, String
 from enum import Enum
-from system_monitor import SystemMonitor
+import subprocess, time
+
+class SystemMonitor:
+    def __init__(self):
+        self.ip = ""
+        self.cpu = ""
+        self.mem_usage = ""
+        self.disk = ""
+        self.temperature = ""
+        self.paused = False
+        self.update() # set initial values.
+
+    def update(self):
+        # Get IP address
+        cmd = "hostname -I | cut -d\' \' -f1 | head --bytes -1"
+        ipString = subprocess.check_output(cmd, shell=True).decode().strip()
+        self.ip = f"IP: {ipString}"
+
+        # Get CPU usage
+        cmd = "top -bn1 | grep load | awk '{printf \"%.2fLA\", $(NF-2)}'"
+        cpuString = subprocess.check_output(cmd, shell=True).decode().strip()
+        self.cpu = f"CPU: {cpuString}"
+
+        # Get memory usage
+        cmd = "free -m | awk 'NR==2{printf \"%.2f%%\", $3*100/$2 }'"
+        memString = subprocess.check_output(cmd, shell=True).decode().strip()
+        self.mem_usage = f"MEM: {memString}"
+
+        # Get disk usage
+        cmd = "df -h | awk '$NF==\"/\"{printf \"%d/%dGB\", $3,$2}'"
+        diskString = subprocess.check_output(cmd, shell=True).decode().strip()
+        self.disk = f"DISK: {diskString}"
+
+        # Get temperature
+        cmd = "vcgencmd measure_temp | cut -d '=' -f 2 | head --bytes -1"
+        tempString = subprocess.check_output(cmd, shell=True).decode().strip()
+        self.temperature = f"TEMP: {tempString}"
+
+    def pause(self):
+        self.paused = True
+
+    def resume(self):
+        self.paused = False
+
 
 RED = ColorRGBA(r=255.0, g=0.0, b=0.0, a=1.0)
 GREEN = ColorRGBA(r=0.0, g=255.0, b=0.0, a=1.0)
