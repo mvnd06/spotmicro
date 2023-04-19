f1514bd687d3bb120cce07a2339ec4f97634b5b5
Armand Raynor <13355630+mvnd06@users.noreply.github.com>
2023-04-19 14:05:29 -0700

    0.4

--- system_status/src/system_status.py
+++ system_status/src/system_status.py
@@ -8,11 +8,9 @@ import subprocess
 def get_service_status(service_name):
     cmd = ['systemctl', 'is-active', service_name]
     try:
-        rospy.loginfo(f"Trying System: {service_name}")
         subprocess.check_output(cmd)
         return 1
     except subprocess.CalledProcessError:
-        rospy.loginfo(f"Exception..")
         return 0
 
 def main():
@@ -23,25 +21,22 @@ def main():
     # Create publishers
     motion_pub = rospy.Publisher('/motion_status', Int32, queue_size=10)
     display_pub = rospy.Publisher('/display_status', Int32, queue_size=10)
-    gui_pub = rospy.Publisher('/gui_status', Int32, queue_size=10)
-    
-    rospy.loginfo(f"Created Publishers..")
+    bridge_pub = rospy.Publisher('/rosbridge_status', Int32, queue_size=10)
 
     rate = rospy.Rate(5)  # 5 Hz
 
     while not rospy.is_shutdown():
-        rospy.loginfo(f"Started Loop...")
         # Get service status
         motion_status = get_service_status('motion')
         display_status = get_service_status('display')
-        gui_status = get_service_status('gui')
+        rosbridge_status = get_service_status('rosbridge')
 
-        rospy.loginfo(f"Motion: {motion_status}, Display: {display_status}, GUI: {gui_status}")
+        rospy.loginfo(f"Motion: {motion_status}, Display: {display_status}, Bridge: {rosbridge_status}")
 
         # Publish status messages
         motion_pub.publish(motion_status)
         display_pub.publish(display_status)
-        gui_pub.publish(gui_status)
+        bridge_pub.publish(rosbridge_status)
 
         rate.sleep()
 
