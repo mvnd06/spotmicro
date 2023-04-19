0e5872b8ac1de3697219c85128bcbc81e6cd32cc
Armand Raynor <13355630+mvnd06@users.noreply.github.com>
2023-04-19 14:03:12 -0700

    0.3

--- system_status/src/system_status.py
+++ system_status/src/system_status.py
@@ -8,9 +8,11 @@ import subprocess
 def get_service_status(service_name):
     cmd = ['systemctl', 'is-active', service_name]
     try:
+        rospy.loginfo(f"Trying System: {service_name}")
         subprocess.check_output(cmd)
         return 1
     except subprocess.CalledProcessError:
+        rospy.loginfo(f"Exception..")
         return 0
 
 def main():
@@ -22,10 +24,13 @@ def main():
     motion_pub = rospy.Publisher('/motion_status', Int32, queue_size=10)
     display_pub = rospy.Publisher('/display_status', Int32, queue_size=10)
     gui_pub = rospy.Publisher('/gui_status', Int32, queue_size=10)
+    
+    rospy.loginfo(f"Created Publishers..")
 
     rate = rospy.Rate(5)  # 5 Hz
 
     while not rospy.is_shutdown():
+        rospy.loginfo(f"Started Loop...")
         # Get service status
         motion_status = get_service_status('motion')
         display_status = get_service_status('display')
